f718b42e5cbc0a9115ad004b5fdd3cca1716b9a1
shghyghm <mohajerishaqayeq@gmail.com>
2026-03-04 11:34:41 +0330

    Add JWT authentication dependency

--- app/services/security.py
+++ app/services/security.py
@@ -1,6 +1,13 @@
 from datetime import datetime, timedelta
 from jose import jwt
 from pwdlib import PasswordHash
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from jose import JWTError, jwt
+from ..database import get_db
+from .. import models
+
 
 SECRET_KEY = "change_this_secret_key"
 ALGORITHM = "HS256"
@@ -22,3 +29,30 @@ def create_access_token(data: dict):
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    db: Session = Depends(get_db)
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+    )
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+
+    user = db.query(models.User).filter(models.User.username == username).first()
+    if user is None:
+        raise credentials_exception
+
+    return user
